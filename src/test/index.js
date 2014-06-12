3983cc7a8780d732d396cebf81728f6806938d88
Debjeet Biswas <debjeet@vxtindia.com>
2014-06-12 08:14:01 +0530

    Changed name of environment variable

--- src/test/index.js
+++ src/test/index.js
@@ -1,11 +1,11 @@
 var Magento = require('../magento');
 
-if (!process.env.MAGENTO_STORE_URL) throw new Error('Store url not present in environment variable MAGENTO_STORE_URL');
+if (!process.env.MAGENTO_HOST) throw new Error('Host not present in environment variable MAGENTO_HOST');
 if (!process.env.MAGENTO_USER) throw new Error('Username not present in environment variable MAGENTO_USER');
 if (!process.env.MAGENTO_PASS) throw new Error('Password not present in environment variable MAGENTO_PASS');
 
 var magento = new Magento({
-	host: process.env.MAGENTO_STORE_URL,
+	host: process.env.MAGENTO_HOST,
 	port: 80,
 	path: '/api/xmlrpc/',
 	login: process.env.MAGENTO_USER,
